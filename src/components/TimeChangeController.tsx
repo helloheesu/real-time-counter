91f898c1542a4075192e0d131fe8e435bea7782b
helloheesu <heesu.jung.developer@gmail.com>
2024-05-27 21:51:01 +0900

    fix error

--- src/components/TimeChangeController.tsx
+++ src/components/TimeChangeController.tsx
@@ -29,7 +29,7 @@ const TimeChangeController = ({
         <TogglableButton
           disabled={disabled}
           onClick={() => {
-            onDelta(-inputTime);
+            onDelta(inputTime);
           }}
         >
           ↑
@@ -37,7 +37,7 @@ const TimeChangeController = ({
         <TogglableButton
           disabled={disabled}
           onClick={() => {
-            onDelta(inputTime);
+            onDelta(-inputTime);
           }}
         >
           ↓
