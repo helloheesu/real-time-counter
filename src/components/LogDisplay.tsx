a9c26b0428ae9cc5f97dee6d6af8f47c9d6321b2
helloheesu <heesu.jung.developer@gmail.com>
2024-05-27 20:52:56 +0900

    table display

--- src/components/LogDisplay.tsx
+++ src/components/LogDisplay.tsx
@@ -1,13 +1,18 @@
 import { LogsContext } from '@/MemberContext';
 import { useContext } from 'react';
 import { formatTime, pad } from '@/timeUtil';
+import React from 'react';
 
 const LogDisplay = () => {
   const { logs } = useContext(LogsContext);
   return (
     <div className="flex flex-col items-center">
-      <h1>Log</h1>
-      <div className="flex flex-col items-center">
+      <h1 className="mb-2">Log</h1>
+      <div className="grid gap-2 grid-cols-[4rem_2rem_4rem_4rem]">
+        <span>이름</span>
+        <span className="text-right">추가</span>
+        <span>절대시간</span>
+        <span>상대시간</span>
         {logs.map(({ member, delta, absoluteTimestamp, currentTimestamp }) => {
           const { minutes: absMin, seconds: absSec } =
             formatTime(absoluteTimestamp);
@@ -15,11 +20,12 @@ const LogDisplay = () => {
             formatTime(currentTimestamp);
 
           return (
-            <div key={absoluteTimestamp}>
-              <span>{member}</span> / <span>{formatTime(delta).minutes}분</span>{' '}
-              / <span>{`${pad(absMin)}:${pad(absSec)}`}</span> /{' '}
+            <React.Fragment key={absoluteTimestamp}>
+              <span>{member}</span>
+              <span className="text-right">{formatTime(delta).minutes}분</span>
+              <span>{`${pad(absMin)}:${pad(absSec)}`}</span>
               <span>{`${pad(curMin)}:${pad(curSec)}`}</span>
-            </div>
+            </React.Fragment>
           );
         })}
       </div>
