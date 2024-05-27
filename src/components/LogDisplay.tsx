0b509b5bc20e9500b797cc55fa9613ebd8f8ef9a
helloheesu <heesu.jung.developer@gmail.com>
2024-05-27 21:56:58 +0900

    show after result

--- src/components/LogDisplay.tsx
+++ src/components/LogDisplay.tsx
@@ -8,16 +8,20 @@ const LogDisplay = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="mb-2">Log</h1>
-      <div className="grid gap-2 grid-cols-[4rem_4rem_4rem_4rem]">
+      <div className="grid gap-2 grid-cols-5">
         <span>이름</span>
         <span>추가</span>
         <span>절대시간</span>
         <span>상대시간</span>
+        <span>변경 후</span>
         {logs.map(({ member, delta, absoluteTimestamp, currentTimestamp }) => {
           const { minutes: absMin, seconds: absSec } =
             formatTime(absoluteTimestamp);
           const { minutes: curMin, seconds: curSec } =
             formatTime(currentTimestamp);
+          const { minutes: afterMin, seconds: afterSec } = formatTime(
+            currentTimestamp + delta
+          );
 
           return (
             <React.Fragment key={absoluteTimestamp}>
@@ -25,6 +29,7 @@ const LogDisplay = () => {
               <span>{formatTime(delta).minutes}분</span>
               <span>{`${pad(absMin)}:${pad(absSec)}`}</span>
               <span>{`${pad(curMin)}:${pad(curSec)}`}</span>
+              <span>{`${pad(afterMin)}:${pad(afterSec)}`}</span>
             </React.Fragment>
           );
         })}
